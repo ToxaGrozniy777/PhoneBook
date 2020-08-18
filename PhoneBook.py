9a449f1f124247258a01d6363e92cbb49c7f7432
Anton <starkovanton488@gmail.com>
2020-08-18 12:56:37 +0300

    добавление удаление записи

--- PhoneBook.py
+++ PhoneBook.py
@@ -19,27 +19,30 @@ while True:
 
     if choice == 1:
         print(phone_book)
-    elif choice == 2: 
+    elif choice == 2:
         tel = input("Введите номер телефона: ")
         if tel in phone_book:
             print("Такой номер уже существует")
             continue
-        value = input_data()
-        phone_book[tel] = value
+        else:
+            value = input_data()
+            phone_book[tel] = value
     elif choice == 3:  # TODO Редактирование записи
         print()
-    elif choice == 4:  # TODO Удаление записи
-        print()
+    elif choice == 4:
+        tel = input("Введите номер телефона для удаления: ")
+        if tel in phone_book:
+            note = phone_book.pop(tel)
+            print('Запись', note, 'удалена')
+        else:
+            print("Вы ввели неправильный номер")
+            continue
     elif choice == 0:
         print("До свидания")
         break
     else:
-        print("Непрвильный режим")
+        print("Неправильный режим")
         continue
-
-
-
-
 tel = input("Введите номер телефона: ")
 value = input_data()
 
