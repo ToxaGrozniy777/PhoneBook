17a300c38100c588a1e8c3212ad8d65b19fad242
Anton <starkovanton488@gmail.com>
2020-08-19 11:21:18 +0300

    добавление журанлирования

--- phonebook_lib.py
+++ phonebook_lib.py
@@ -1,8 +1,29 @@
 
+# *** PhoneBook ***
+#
+# tel - номер телефона, строка
+# first_name - имя, строка
+# last_name - фамилия, строка
+# patronymic - отчество, строка
+# address - адрес, строка
+# {tel: [last_name, first_name, patronymic, address]}
+
+import datetime
+
+date_time = datetime.datetime.now()
+
+
+def log(msg):
+    with open("PhoneBook.log", "a") as file:
+        message = date_time.strftime("%Y-%m-%d %H:%M") + " : " + msg + "\n"
+        file.write(message)
+
+
 def welcome():
     print("*******")
     print("*** PhoneBook - телефонный справочник ***")
     print("*******")
+    log("Программа запустилась")
 
 
 def menu():
@@ -36,16 +57,19 @@ def show(phone_book):
         temp = value[0] + " " + value[1] + " " + value[2] + ", " + value[3]
         print(tel, ':', temp)
     print("--- --- ---")
+    log("Вывод справочника на экран")
 
 
 def input_record(phone_book):
     tel = input("Введите номер телефона: ")
     if tel in phone_book:
         print("# Такой номер уже существует #")
+        log("Неудачная попытка добавления записи с номером " + tel)
     else:
         value = input_data()
         phone_book[tel] = value
         print("# Запись успешно добавлена #")
+        log("Запись с " + tel + " успешно добавлена")
 
 
 def edit_record(phone_book):
@@ -54,8 +78,11 @@ def edit_record(phone_book):
         temp = input_data()
         phone_book[tel] = temp
         print("# Запись успешно изменена #")
+        log("Запись с " + tel + " успешно изменена")
     else:
         print("# Вы ввели неправильный номер #")
+        log("Неудачная попытка редактирования записи с номером " + tel)
+
 
 
 def delete_record(phone_book):
@@ -63,8 +90,10 @@ def delete_record(phone_book):
     if tel in phone_book:
         phone_book.pop(tel)
         print("# Запись " + tel + " удалена #")
+        log("Запись с " + tel + " успешно удалена")
     else:
         print("# Вы ввели неправильный номер #")
+        log("Неудачная попытка удаления записи с номером " + tel)
 
 
 def export_to_file(phone_book):
@@ -73,3 +102,4 @@ def export_to_file(phone_book):
             value = phone_book[tel]
             temp = tel + ";" + value[0] + ";" + value[1] + ";" + value[2] + ";" + value[3] + "\n"
             file.write(temp)
+    log("Экспорт в файл")
